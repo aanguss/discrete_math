41cf488667e4b6c70bd39dc90985708ac76e8e28
aanguss <aangus@gmail.com>
2020-05-10 21:51:51 -0700

    debug for mod determined to be % to mean only remainder

--- module2/ch2_sect1_prob3_page72.c
+++ module2/ch2_sect1_prob3_page72.c
@@ -13,23 +13,44 @@
  * Problem 3 - Page 72
  */
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 /* defines for type of caeser cipher */
 #define SHIFT_AMOUNT 3
+#define ASCII_START_INDEX 65
+#define ALPHABET_SIZE 26
+
+/* calculate true modulo  to handle negative values */
+int mod(int val, int modVal) {
+    int remainder = val % modVal;
+    
+    /* make sure mod is actually returned when negative value calculated */
+    if (remainder < 0) {
+        remainder += modVal;
+    } 
+
+    return remainder;
+}
 
 /* caeser cipher encode in main function */
 int main(void) {
-    char lowerAlphabet[27] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-    char upperAlphabet[27] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    
     char * userString = "HERE IS A MESSAGE";
 
-    printf("\n%s is encoded as: ", userString);
+    printf("\n\"%s\" is encoded as: ", userString);
     for (int i = 0; i < strlen(userString); i++) {
+        printf("\n");
+        printf("%c[%d]->", userString[i], userString[i]);
         if (userString[i] == ' ') {
             printf("%c", userString[i]);
         } else {
-            printf("%c", upperAlphabet[ (userString[i] - SHIFT_AMOUNT) % 26 ]);
+            int alphaIndex = (((int)userString[i] - ASCII_START_INDEX - SHIFT_AMOUNT) % ALPHABET_SIZE) + ASCII_START_INDEX;
+            int al = (int)userString[i] - ASCII_START_INDEX - SHIFT_AMOUNT;
+            int alpha = mod(al, ALPHABET_SIZE);
+            printf("%d->", al);
+            printf("%d->", alpha);
+            printf("%c[%d]", alphaIndex, alphaIndex);
         }
     }
     printf("\n\n");
