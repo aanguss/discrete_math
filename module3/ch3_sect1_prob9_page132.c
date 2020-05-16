bfb6f928fe4397a7fcdb5207362f3126d63ef105
Anthony Angus <anthony.angus@intel.com>
2020-05-16 16:06:47 -0700

    changed end of 3.1-9 to include equivalent column

--- module3/ch3_sect1_prob9_page132.c
+++ module3/ch3_sect1_prob9_page132.c
@@ -28,7 +28,8 @@ int main(void) {
 
     // add some space and print a header for the first truth table
     printf("\n");
-    printf("s\tt\t!s\t!(s||t)\t!(s||!t)\t!(s||t)&&!(s||!t)\n");
+    // printf("s\tt\t!s\t!(s||t)\t!(s||!t)\t!(s||t)&&!(s||!t)\n");
+    printf("s\tt\t!s\t!(s||t)&&!(s||!t)\tEQUIVALENT\n");
     
     // go through each iteration of s and t and print results
     for (s = 0; s < 2; s++) {
@@ -39,7 +40,8 @@ int main(void) {
             s_or_tnot = s || !t;
             not_s_or_tnot = !s_or_tnot;
             final = not_s_or_t || not_s_or_tnot;
-            printf("%d\t%d\t%d\t%d\t%d\t\t%d\n", s, t, snot, not_s_or_t, not_s_or_tnot,final);
+            // printf("%d\t%d\t%d\t%d\t%d\t\t%d\n", s, t, snot, not_s_or_t, not_s_or_tnot,final);
+            printf("%d\t%d\t%d\t%d\t\t\t%s\n", s, t, snot, final, final == snot?"TRUE":"FALSE");
         }
     }
 
