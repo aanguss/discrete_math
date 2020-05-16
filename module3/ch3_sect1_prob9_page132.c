69b2f00097b28ace96bb235c8172a47b9ba41ae7
Anthony Angus <anthony.angus@intel.com>
2020-05-16 15:58:33 -0700

    added 3.1-9

--- module3/ch3_sect1_prob9_page132.c
+++ module3/ch3_sect1_prob9_page132.c
@@ -20,8 +20,28 @@
 
 /* main function */
 int main(void) {
-    
+    // needed variables
+    int s, t, snot;
+    int s_or_t, not_s_or_t;
+    int s_or_tnot, not_s_or_tnot;
+    int final;
 
+    // add some space and print a header for the first truth table
+    printf("\n");
+    printf("s\tt\t!s\t!(s||t)&&!(s||!t)\n");
+    
+    // go through each iteration of s and t and print results
+    for (s = 0; s < 2; s++) {
+        for (t = 0; t < 2; t++) {
+            snot = !s;
+            s_or_t = s || t;
+            not_s_or_t = !s_or_t;
+            s_or_tnot = s || !t;
+            not_s_or_tnot = !s_or_tnot;
+            final = not_s_or_t || not_s_or_tnot;
+            printf("%d\t%d\t%d\t%d\n", s, t, snot, not_s_or_t, not_s_or_tnot,final);
+        }
+    }
 
     return 0;
 }
