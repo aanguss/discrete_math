8d23887286bca860f413d29ed4a33eacc1e5c087
Anthony Angus <anthony.angus@intel.com>
2020-05-16 16:00:54 -0700

    fix formatting

--- module3/ch3_sect1_prob9_page132.c
+++ module3/ch3_sect1_prob9_page132.c
@@ -28,7 +28,7 @@ int main(void) {
 
     // add some space and print a header for the first truth table
     printf("\n");
-    printf("s\tt\t!s\t!(s||t)&&!(s||!t)\n");
+    printf("s\tt\t!s\t!(s||t)\t!(s||!t)\t!(s||t)&&!(s||!t)\n");
     
     // go through each iteration of s and t and print results
     for (s = 0; s < 2; s++) {
@@ -39,7 +39,7 @@ int main(void) {
             s_or_tnot = s || !t;
             not_s_or_tnot = !s_or_tnot;
             final = not_s_or_t || not_s_or_tnot;
-            printf("%d\t%d\t%d\t%d\n", s, t, snot, not_s_or_t, not_s_or_tnot,final);
+            printf("%d\t%d\t%d\t%d\t%d\t\t%d\n", s, t, snot, not_s_or_t, not_s_or_tnot,final);
         }
     }
 
