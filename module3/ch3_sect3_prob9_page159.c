e678c41402f89edb22017e472cbfdf504d81f28c
Anthony Angus <anthony.angus@intel.com>
2020-05-16 17:35:32 -0700

    added 3.3-9

--- module3/ch3_sect3_prob9_page159.c
+++ module3/ch3_sect3_prob9_page159.c
@@ -14,10 +14,38 @@
 #include <stdio.h>
 #include <string.h>
 
+/* exponent helper function for simple use with output up to 2,147,483,647 */
+int power(int val, int exp) {
+    int newVal = 1;
+
+    for (int i = 0; i < exp; i++) {
+        newVal *= val;
+    }
+
+    return newVal;
+}
+
 /* main function */
 int main(void) {
-    
+    // needed variables
+    int x;
+    double x_cubed;
 
+    // space at the start
+    printf("\nPROBLEM looking for FALSE\n");
+
+    // problem a - go through each iteration and stop if false found
+    for (x = 3; x <= 0xFF; x++) {
+        x_cubed = power(x,3);
+        printf("%d^3 = %0.f\t%s\n", x, x_cubed, (x_cubed <= 8)?"TRUE":"FALSE");
+        if ((x_cubed <= 8)) {
+            printf("\n\nNOT TRUE: x = %d and %d^3 = %0.f\n", x, x, x_cubed);
+            break;
+        }
+    }
+
+    // space at the end
+    printf("\n");
 
     return 0;
 }
