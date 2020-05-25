8e737acdbfe44625c2b190547d38e8d906a51925
aangus <anthony.angus@intel.com>
2020-05-25 14:26:53 -0700

    4.1-3 started, but need to move to linux

--- module4/ch4_sect1_prob3_page181.c
+++ module4/ch4_sect1_prob3_page181.c
@@ -15,15 +15,46 @@
 #include <stdio.h>
 #include <string.h>
 
+/* recursive function */
+int recursiveTotal(int n) {
+    int runningTotal;
+
+    /* no value if less than 1 */
+    if (n < 1) {
+        return 0;
+    }
+
+    /* base condition is 1, return when found n = 1 */
+    if (n == 1) {
+        return 1;
+    }
+
+    runningTotal = n*(n+1);
+    runningTotal = runningTotal + recursiveTotal(n - 1);
+
+    return runningTotal;
+}
+
 /* main function */
 int main(void) {
     // needed variables
-    
+    int sum, rsum;
 
     // add some space and print a header for the first truth table
     printf("\n");
     
-
+    /* find and print sum with n(n+1)(n+2)/3 */
+    for (int i = 0; i < 10; i++) {
+        sum = i*(i+1)*(i+2)/3;
+        rsum = recursiveTotal(i);
+        printf("----%d----\n", i);
+        printf("%d(%d+1)(%d+2)/3 = %d\n", i, i, i, sum);
+        printf("recursive %d(%d+1) = %d\n", i, i, i, sum);
+        if (sum != rsum) {
+            printf("ERROR: sum and rsum do not equal.\n");
+        }
+        printf("\n");
+    }
     
     printf("\n");
 
