a767c4d0df2fa6f83437faf989450643c18f1f39
Tony <aangus@gmail.com>
2020-06-08 01:54:57 -0700

    5.2-5 added

--- module6/ch5_sect2_prob5_page274.c
+++ module6/ch5_sect2_prob5_page274.c
@@ -14,9 +14,84 @@
  */
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <time.h>
+#include <stdint.h>
 
 int main(void)
 {
     //Grab random card and check probability over 1000 draws
+    time_t t;
+    uint32_t iterations = 1000;
+    uint32_t cardDraws[iterations]; // 100 iterations, 4 types, 13 of each
+    uint32_t black = 0;
+    uint32_t diamond = 0;
+    uint32_t ace = 0;
+    uint32_t diamond_ace = 0;
+    uint32_t black_ace = 0;
+    uint32_t black_or_diamond_or_ace = 0;
+    
+    // start with seperation so results are easier to see
+    printf("\n");
+    printf("draw from %d fresh decks and track the card pulled:\n", iterations);
+
+    // start randomness with current time
+    srand((unsigned) time(&t));
+
+    // null out all values in deck
+    for (int i = 0; i < iterations; i++) {
+        cardDraws[i] = 0;
+    }
+
+    // null out all values in deck
+    for (int i = 0; i < iterations; i++) {
+        cardDraws[i] = (rand() % 53) + 1;
+        // clubs 1 - 13
+        // spades 14 - 26
+        // diamonds 27 - 39
+        // hearts 40 - 52
+        if (cardDraws[i] >= 1 && cardDraws[i] <= 13) {
+            black++;
+            black_or_diamond_or_ace++;
+            if (cardDraws[i] == 13) {
+                ace++;
+                black_ace++;
+            }
+        } else if (cardDraws[i] >= 14 && cardDraws[i] <= 26) {
+            black++;
+            black_or_diamond_or_ace++;
+            if (cardDraws[i] == 26) {
+                ace++;
+                black_ace++;
+            }
+        } else if (cardDraws[i] >= 27 && cardDraws[i] <= 39) {
+            diamond++;
+            black_or_diamond_or_ace++;
+            if (cardDraws[i] == 39) {
+                ace++;
+                diamond_ace++;
+            }
+        } else if (cardDraws[i] >= 40 && cardDraws[i] <= 52) {
+            if (cardDraws[i] == 52) {
+                ace++;
+                black_or_diamond_or_ace++;
+            }
+        }
+    }
+
+    // let's print all the values out
+    for (int i = 0; i < iterations; i++) {
+        printf("%d, ", cardDraws[i]);
+    }
+
+    printf("\n");
+    printf("found results for each type:\n");
+    printf("ace card: %d, gives P(ace) = %f\n", ace, (float)ace/iterations);
+    printf("black card: %d, gives P(black) = %f\n", black, (float)black/iterations);
+    printf("black ace card: %d, gives P(black_ace) = %f\n", black_ace, (float)black_ace/iterations);
+    printf("diamond card: %d, gives P(diamond) = %f\n", diamond, (float)diamond/iterations);
+    printf("diamond ace card: %d, gives P(diamond_ace) = %f\n", diamond_ace, (float)diamond_ace/iterations);
+    printf("black or diamond or ace card: %d, gives P(black_or_diamond_or_ace) = %f\n", black_or_diamond_or_ace, (float)black_or_diamond_or_ace/iterations);
+
+    // some space
+    printf("\n");
 }
--- module6/ch5_sect3_prob7_page291.c
+++ module6/ch5_sect3_prob7_page291.c
@@ -18,10 +18,73 @@
  */
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <time.h>
+#include <stdint.h>
 
 int main(void)
 {
     //Simulate the randomness of getting the coins out… and output
+    time_t t;
+    uint32_t heads = 0;
+    uint32_t tails = 0;
     
+    // start with seperation so results are easier to see
+    printf("\n");
+    printf("flip a coin 1000 times, results are:\n");
+
+    // start randomness with current time
+    srand((unsigned) time(&t));
+
+    // go through 1000 coins and find probability of each heads and tails
+    for (int i = 0; i < 1000; i++) {
+        if (((rand() % 2) + 1) == 2) {
+            heads++;
+        } else {
+            tails++;
+        }
+    }
+    printf("heads: %d\n", heads);
+    printf("tails: %d\n", tails);
+    
+    printf("\n");
+    printf("now let's flip a coin 5 times and find out how many times heads lands 3 times:\n");
+    uint32_t five_flips[100][5];
+
+    // initialize all the coin flips to -1
+    for (int i = 0; i < 100; i++) {
+        for (int j = 0; j < 5; j++) {
+            five_flips[i][j] = 0;
+        }
+    }
+
+    // now add a coin value for each
+    for (int i = 0; i < 100; i++) {
+        for (int j = 0; j < 5; j++) {
+            five_flips[i][j] = ((rand() % 2) + 1);
+        }
+    }
+
+    // let's print them for funzies
+    int num3Heads = 0;
+    int num3PlusHeads = 0;
+    for (int i = 0; i < 100; i++) {
+        int numHeads = 0;
+        for (int j = 0; j < 5; j++) {
+            if (five_flips[i][j] == 2) {
+                numHeads++;
+            }
+            printf("%c, ", five_flips[i][j]==1?'H':'T');
+        }
+        if (numHeads == 3) { num3Heads++; }
+        if (numHeads >= 3) { num3PlusHeads++; }
+        printf("\n"); // new line for each five flips
+    }
+
+    // print results
+    printf("number of 3 heads: %d, which gives P(H) = %f\n", num3Heads, (float)num3Heads/100);
+    printf("number of at least 3 heads: %d, which gives P(H) = %f\n", num3PlusHeads, (float)num3PlusHeads/100);
+
+    // some space
+    printf("\n");
+}
 }
